c48295c35204491cc40120c5215ae42bcf6e7b8e
Осипов Андрей Алексеевич <AOsipov@vertex.spb.ru>
2020-09-02 16:30:25 +0300

    Added test to List and Detail views

--- bboard_api/main/tests.py
+++ bboard_api/main/tests.py
@@ -1,4 +1,5 @@
 import random
+from datetime import timedelta, datetime
 
 from django.test import TestCase
 from django.urls import reverse
@@ -15,10 +16,26 @@ def bulletin_create(**kwargs):
     )
 
 
-class BulletinsListViewTests(TestCase):
+D1 = datetime.strptime('1/1/2008 1:30 PM', '%m/%d/%Y %I:%M %p')
+D2 = datetime.strptime('1/1/2020 4:50 AM', '%m/%d/%Y %I:%M %p')
 
+
+def random_date(start, end):
+    """
+    This function will return a random datetime between two datetime
+    objects.
+    """
+    delta = end - start
+    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+    random_second = random.randrange(int_delta)
+    return start + timedelta(seconds=random_second)
+
+
+class BulletinsListViewTests(TestCase):
+    """ Тестирование выдачи списка объявлений """
     @staticmethod
     def create_test_bulletins(count):
+        """ Создаем сразу несколько объявлений в БД """
         for _ in range(count):
             bulletin_create(
                 name='test bulletin',
@@ -28,16 +45,19 @@ class BulletinsListViewTests(TestCase):
             )
 
     def get_bulletins_response(self, url=None):
+        """ Получаем объявления по API """
         if url:
             return self.client.get(url)
         return self.client.get(reverse('main:bulletins'))
 
     def test_no_bulletins(self):
+        """ В БД нет объявлений """
         response = self.client.get(reverse('main:bulletins'))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data['count'], 0)
 
     def test_one_bulletin(self):
+        """ В БД одно объявление """
         bulletin = bulletin_create(
             name='test bulletin',
             price=115,
@@ -52,12 +72,14 @@ class BulletinsListViewTests(TestCase):
             self.assertEqual(value, bulletin.__getattribute__(field))
 
     def test_many_bulletins(self):
+        """ В БД несколько объявлений """
         n_bulletins = random.randint(10, 40)
         self.create_test_bulletins(n_bulletins)
         response = self.get_bulletins_response()
         self.assertEqual(response.data['count'], n_bulletins)
 
     def test_paginated_bulletins(self):
+        """ Проверка пагинации """
         n_bulletins = random.randint(11, 19)
         self.create_test_bulletins(n_bulletins)
         response = self.get_bulletins_response()
@@ -71,7 +93,69 @@ class BulletinsListViewTests(TestCase):
         )
 
     def test_price_sorting_bulletins(self):
-        pass
+        """ Тестирование сортировки объявлений по имени в разные направления """
+        for number in range(10):
+            bulletin_create(
+                name=number,
+                price=number,
+                main_photo=number,
+                description=number
+            )
+        response = self.client.get(reverse('main:bulletins') + '?sort=name')
+        results = response.data['results']
+        desc_response = self.client.get(reverse('main:bulletins') + '?sort=name&desc')
+        desc_results = desc_response.data['results']
 
-    def test_data_sorting_bulletins(self):
-        pass
+        self.assertEqual(list(reversed(results)), list(desc_results))
+
+    def test_date_sorting_bulletins(self):
+        """ Тестирование сортировки объявлений по дате в разные направления """
+        for number in range(10):
+            Bulletin.objects.create(
+                name=number,
+                price=number,
+                main_photo=number,
+                date=random_date(D1, D2),
+                description=number
+            )
+        response = self.client.get(reverse('main:bulletins') + '?sort=date')
+        results = response.data['results']
+        desc_response = self.client.get(reverse('main:bulletins') + '?sort=date&desc')
+        desc_results = desc_response.data['results']
+
+        self.assertEqual(list(results), list(reversed(desc_results)))
+
+
+class BulletinDetailView(TestCase):
+
+    @staticmethod
+    def create_bulletins():
+        n_bulletins = random.randint(10, 20)
+        for number in range(n_bulletins):
+            bulletin_create(
+                name=number,
+                price=number,
+                main_photo=number,
+                description=number
+            )
+        return n_bulletins
+
+    def test_necessary_fields(self):
+        """ В выдаче только обязательные поля объявления """
+        n_bulletins = self.create_bulletins()
+        pk = random.randint(10, n_bulletins)
+
+        response = self.client.get(reverse('main:bulletin', kwargs={'pk': pk}))
+        self.assertEqual(len(response.data), 3)
+        self.assertIn('name', response.data)
+        self.assertIn('price', response.data)
+        self.assertIn('main_photo', response.data)
+
+    def test_all_fields(self):
+        """ В выдаче все поля объявления """
+        n_bulletins = self.create_bulletins()
+        pk = random.randint(10, n_bulletins)
+        response = self.client.get(reverse('main:bulletin', kwargs={'pk': pk}) + '?fields')
+
+        for field in Bulletin._meta.fields:
+            self.assertIn(field.attname, response.data)
